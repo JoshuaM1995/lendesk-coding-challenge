9cb753cbc1af52a8e2700f8886b75a91f543c612
Joshua McNabb <joshuamcnabb1995@gmail.com>
2023-10-10 14:40:00 -0600

    add redis to nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,28 @@
+import { AuthController } from './modules/auth/auth.controller';
+import { AuthModule } from './modules/auth/auth.module';
 import { Module } from '@nestjs/common';
+import { AuthService } from './modules/auth/auth.service';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        config: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+          username: configService.get('REDIS_USERNAME'),
+          password: configService.get('REDIS_PASSWORD'),
+        },
+      }),
+    }),
+    AuthModule,
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AppModule {}
