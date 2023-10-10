d741d981327436a10822aac6a440d34d2aff7a87
Joshua McNabb <joshuamcnabb1995@gmail.com>
2023-10-10 14:12:01 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000);
+
+  console.log('Server running on http://localhost:4000');
+}
+
+bootstrap();
