ba3d8ec96f825c2279c2390e95d9e00e28163cdb
Joshua McNabb <joshuamcnabb1995@gmail.com>
2023-10-10 20:47:10 -0600

    add config module validation

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,10 +3,20 @@ import { AuthModule } from '@modules/auth';
 import { UserModule } from '@modules/user';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        JWT_SECRET: Joi.string().required(),
+        REFRESH_TOKEN_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+        REFRESH_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
     RedisModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
