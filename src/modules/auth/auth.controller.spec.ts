084ca46fd123074803418ede912d1ef39d16db85
Joshua McNabb <joshuamcnabb1995@gmail.com>
2023-10-11 17:30:51 -0600

    ensure /auth/login throws a 401 when the user isn't found

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,20 +1,27 @@
 import { faker } from '@faker-js/faker';
 import { UserService } from '@modules/user';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import {
+  HttpStatus,
+  INestApplication,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { setupTests } from '@utils/setup-tests';
 import * as request from 'supertest';
 import { v4 as uuidv4 } from 'uuid';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let app: INestApplication;
   let userService: UserService;
+  let authService: AuthService;
 
   beforeAll(async () => {
     app = await setupTests();
-    userService = app.get<UserService>(UserService);
+    userService = app.get(UserService);
+    authService = app.get(AuthService);
   });
 
-  describe('POST /register', () => {
+  describe('POST /auth/register', () => {
     it("should create a user when the username and password requirements are met, and the user doesn't exist", async () => {
       const mockUsername = faker.internet.userName();
       const mockPassword = 'Password123!';
@@ -98,6 +105,26 @@ describe('AuthController', () => {
           password: mockPassword,
         })
         .expect(HttpStatus.CONFLICT);
+    });
+  });
+
+  describe('POST /auth/login', () => {
+    it("should return a 401 when the user isn't found", async () => {
+      const mockUsername = faker.internet.userName();
+      const mockPassword = 'Password123!';
+
+      // Ensure the local strategy throws an error when searching for a user
+      jest.spyOn(authService, 'validateUser').mockImplementation(() => {
+        throw new UnauthorizedException(`Invalid username or password`);
+      });
+
+      await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+          username: mockUsername,
+          password: mockPassword,
+        })
+        .expect(HttpStatus.UNAUTHORIZED);
     });
   });
 });
