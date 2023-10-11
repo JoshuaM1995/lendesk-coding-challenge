a67999ef96e52f1170f6d079230aaae52767a48d
Joshua McNabb <joshuamcnabb1995@gmail.com>
2023-10-10 21:21:06 -0600

    add validation to ensure user id exists

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,21 @@
 import { UserDTO } from '@dtos/user';
-import { Injectable } from '@nestjs/common';
+import { UserService } from '@modules/user';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from 'src/types';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,9 +23,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: UserDTO) {
+  async validate(payload: JwtPayload) {
+    const foundUser = await this.userService.findById(payload.sub);
+
+    if (!foundUser) {
+      throw new UnauthorizedException('User not found');
+    }
+
     return {
-      id: payload.id,
+      id: payload.sub,
       username: payload.username,
     };
   }
