0d1989a7c03dd84c34637a67067e0e134e644a4e
Joshua McNabb <joshuamcnabb1995@gmail.com>
2023-10-24 14:00:00 -0600

    use @modules path alias

--- src/modules/auth/strategies/refresh-token.strategy.ts
+++ src/modules/auth/strategies/refresh-token.strategy.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../../../types';
-import { UserService } from '../../user/user.service';
+import { UserService } from '@modules/user';
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(
